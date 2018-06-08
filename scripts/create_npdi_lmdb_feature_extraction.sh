41d31f2ea8b6f54821339c2ca0b9a58260a1bfb7
murilovarges <murilo.varges@gmail.com>
2018-06-08 17:03:24 -0300

    Implemented script to NPDI-Dataset lmdb file. Implemented script do finetune NPDI-Dataset.

---
+++ scripts/create_npdi_lmdb_feature_extraction.sh
@@ -0,0 +1,10 @@
+# create npdi lists for 5 splits
+i=0
+while [ $i -lt 5 ]; \
+do \
+   python data/create_video_db.py \
+	--list_file=/home/murilo/PycharmProjects/R2Plus1D/data/list/npdi/fold${i}_video_id_dense_l32.txt \
+	--output_file=/home/murilo/PycharmProjects/R2Plus1D/data/lmdb/npdi/fold${i}_video_dense \
+	--use_list=1 --use_video_id=1 --use_start_frame=1
+    i=$((i+1))
+done
